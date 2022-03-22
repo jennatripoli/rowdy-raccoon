4c300e124a81c4fe751bba8d2ee34c32988b1860
JFlynn342 <jflynn2@wpi.edu>
2022-03-22 15:32:21 -0400

    Base project setup part II

---
+++ RaccoonGame/Source/RaccoonGame/RaccoonGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RaccoonGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACCOONGAME_API ARaccoonGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
